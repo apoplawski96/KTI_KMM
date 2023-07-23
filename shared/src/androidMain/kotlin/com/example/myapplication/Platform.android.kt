cfabd1167da2676ac5f847366ac361cde7f77880
artur.poplawski@schibsted.com <artur.poplawski@schibsted.com>
2023-07-23 17:06:32 +0200

    Cleanup pt5

--- shared/src/androidMain/kotlin/com/example/myapplication/Platform.android.kt
+++ shared/src/androidMain/kotlin/com/example/myapplication/Platform.android.kt
@@ -1,23 +1,7 @@
 package com.example.myapplication
 
-import com.example.myapplication.common.JsonFileReader
-import kotlinx.serialization.decodeFromString
-import kotlinx.serialization.json.Json
-
 class AndroidPlatform : Platform {
     override val name: String = "Android ${android.os.Build.VERSION.SDK_INT}"
 }
 
 actual fun getPlatform(): Platform = AndroidPlatform()
-
-actual fun parseQuestionsJson(
-    fileName: String,
-    jsonFileReader: JsonFileReader,
-): List<QuestionSchema> {
-    val jsonFileContent = jsonFileReader.readJsonFile(fileName)
-    return Json.decodeFromString(jsonFileContent)
-}
-
-actual fun readJsonFile(fileName: String): String {
-    return "huj"
-}
--- shared/src/commonMain/kotlin/com/example/myapplication/Platform.kt
+++ shared/src/commonMain/kotlin/com/example/myapplication/Platform.kt
@@ -1,20 +1,7 @@
 package com.example.myapplication
 
-import com.example.myapplication.common.JsonFileReader
-
 interface Platform {
     val name: String
 }
 
 expect fun getPlatform(): Platform
-
-//expect class JsonFileReader {
-//    fun readJsonFile(filePath: String): String
-//}
-
-expect fun readJsonFile(fileName: String): String
-
-expect fun parseQuestionsJson(
-    fileName: String,
-    jsonFileReader: JsonFileReader
-): List<QuestionSchema>
